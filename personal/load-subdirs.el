ca853958f7cf6bc89449863da057c7a11d06500e
Dieuwe de Boer <dieuwe@pobox.com>
2015-08-08 17:48:12 +1200

    Added some comments to the load-subdirs.el file.

--- personal/load-subdirs.el
+++ personal/load-subdirs.el
@@ -1,11 +1,17 @@
-;;; Loads the correct user profile based on the custom-profile variable.
+;;; personal/load-subdirs.el -- summary
 
+;;; Commentary:
+;; Loads the 'common' directory along with the correct user profile
+;; based on the 'custom-profile' variable that should be set in a
+;; user's prelude-modules.el file.
 
-;;; code:
+;;; Code:
 
-;; load subdirectories of configuration.
+;; Load sub directories of configuration.
 (mapc (lambda (dir-name) (let ((subdir (expand-file-name dir-name prelude-personal-dir)))
                            (when (file-exists-p subdir)
                              (message "Loading configuration files in %s..." subdir)
                              (mapc 'load (directory-files subdir 't "^[^#].*el$")))))
       (list "common" custom-profile))
+
+;;; load-subdirs.el ends here
