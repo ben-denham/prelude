de063788a38faa61b6855f347e055ee119e07d6c
Ben Denham <ben.denham@gmail.com>
2016-05-07 10:34:37 +1200

    Add cider-mode key-binding to spit open-scad code from clojure repl into repl.scad.

--- personal/ben/key-bindings.el
+++ personal/ben/key-bindings.el
@@ -5,4 +5,22 @@
 
 ;;; Code:
 
+(global-set-key [M-+] 'menu-bar-open)
+
+;; Add key-binding to spit open-scad code from clojure repl into
+;; repl.scad.
+
+(defun spit-scad-last-expression ()
+  (interactive)
+  (cider-interactive-eval
+   (format
+    "(require 'scad-clj.scad)
+     (spit \"repl.scad\"
+           (scad-clj.scad/write-scad %s))"
+    (cider-last-sexp))))
+
+(require 'cider-mode)
+(define-key cider-mode-map
+  (kbd "C-x C-g") 'spit-scad-last-expression)
+
 ;;; key-bindings.el ends here
