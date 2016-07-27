b5363161681a157da03609c7e56fe22133214d70
Benjamin Denham <ben.denham@gmail.com>
2016-07-27 20:54:14 +1200

    Correct setting of cider-cljs-lein-repl option.

--- personal/common/clojure.el
+++ personal/common/clojure.el
@@ -7,9 +7,9 @@
 
 ;; Configure Cider CLJS REPL to start figwheel-sidecar repl.
 (require 'cider)
-(setq cider-cljs-lein-repl
-      "(do (require 'figwheel-sidecar.repl-api)
-           (figwheel-sidecar.repl-api/start-figwheel!)
-           (figwheel-sidecar.repl-api/cljs-repl))")
+(custom-set-variables
+ '(cider-cljs-lein-repl "(do (require 'figwheel-sidecar.repl-api)
+                         (figwheel-sidecar.repl-api/start-figwheel!)
+                         (figwheel-sidecar.repl-api/cljs-repl))"))
 
 ;;; clojure.el ends here
