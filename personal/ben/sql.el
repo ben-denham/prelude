52a0a9fe34ef0fb258400bb845209cb84fe94f4d
Benjamin Denham <ben.denham@gmail.com>
2017-12-23 10:37:57 +1300

    Add sql configuration

---
+++ personal/ben/sql.el
@@ -0,0 +1,13 @@
+;;; ben/sql.el -- summary
+
+;;; Commentary:
+;; SQL mode configuration.
+
+;;; Code:
+
+;; (prelude-require-package 'sql-indent)
+
+;; (eval-after-load "sql"
+;;       (load-library "sql-indent"))
+
+;;; sql.el ends here
