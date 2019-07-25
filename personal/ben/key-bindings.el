0c5cb59c0bc8bf4226e5d5bb4518c73199f2b60d
Ben Denham <ben@sc.nz>
2019-07-25 13:26:17 +1200

    Add an unfill-region command.

--- personal/ben/key-bindings.el
+++ personal/ben/key-bindings.el
@@ -29,4 +29,15 @@
 (define-key octave-mode-map
   (kbd "C-c C-k") 'octave-send-defun)
 
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
+;; Handy key definition
+(define-key global-map "\C-\M-Q" 'unfill-region)
+
 ;;; key-bindings.el ends here
